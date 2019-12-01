f7f5e989a21021cc4b3bf3f1b3d325d5b4b1f0b6
davidjeremias <com1.com3@gmail.com>
2019-12-01 15:01:29 -0200

    [Changed] crud de cliente

--- src/main/java/br/com/u2d/cadcli/controller/ClienteController.java
+++ src/main/java/br/com/u2d/cadcli/controller/ClienteController.java
@@ -1,10 +1,17 @@
 package br.com.u2d.cadcli.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,8 +26,34 @@ public class ClienteController {
 	@Autowired
 	private ClienteService service;
 	
+	@PostMapping
 	public ResponseEntity<ClienteDTO> salvar(@Valid @RequestBody ClienteDTO clienteDTO){
 		ClienteDTO retorno = service.salvar(clienteDTO);
 		return new ResponseEntity<>(retorno, HttpStatus.CREATED);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<ClienteDTO>> buscar(){
+		List<ClienteDTO> listaRetorno = service.buscar();
+		return !listaRetorno.isEmpty() ? new ResponseEntity<List<ClienteDTO>>(listaRetorno, HttpStatus.OK)
+				: new ResponseEntity<List<ClienteDTO>>(listaRetorno, HttpStatus.NO_CONTENT);
+	}
+	
+	@GetMapping("/{id}/buscaPorId")
+	public ResponseEntity<ClienteDTO> buscaPorId(@PathVariable Integer id){
+		ClienteDTO retorno = service.buscaPorId(id);
+		return retorno != null ? new ResponseEntity<ClienteDTO>(retorno, HttpStatus.OK)
+				: new ResponseEntity<ClienteDTO>(HttpStatus.NO_CONTENT);
+	}
+	
+	@PutMapping
+	public ResponseEntity<ClienteDTO> alterar(@RequestBody ClienteDTO clienteDTO){
+		ClienteDTO retorno = service.alterar(clienteDTO);
+		return new ResponseEntity<ClienteDTO>(retorno, HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/{id}/excluir")
+	public ResponseEntity<ClienteDTO> excluir(@PathVariable Integer id){
+		return null;
 	}
 }
--- src/main/java/br/com/u2d/cadcli/service/ClienteService.java
+++ src/main/java/br/com/u2d/cadcli/service/ClienteService.java
@@ -1,5 +1,9 @@
 package br.com.u2d.cadcli.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,6 +22,26 @@ public class ClienteService {
 	ModelMapper model;
 
 	public ClienteDTO salvar(ClienteDTO clienteDTO) {
+		Cliente cliente = model.map(clienteDTO, Cliente.class);
+		return model.map(repository.save(cliente), ClienteDTO.class);
+	}
+
+	public List<ClienteDTO> buscar() {
+		List<Cliente> listaRetorno = repository.findAll();
+		List<ClienteDTO> listaDTO = new ArrayList<>();
+		listaRetorno.forEach(cliente -> {
+			ClienteDTO dto = model.map(cliente, ClienteDTO.class);
+			listaDTO.add(dto);
+		});
+		return listaDTO;
+	}
+
+	public ClienteDTO buscaPorId(Integer id) {
+		Optional<Cliente> retorno = repository.findById(id);
+		return model.map(retorno.get(), ClienteDTO.class);
+	}
+
+	public ClienteDTO alterar(ClienteDTO clienteDTO) {
 		Cliente cliente = model.map(clienteDTO, Cliente.class);
 		return model.map(repository.save(cliente), ClienteDTO.class);
 	}
